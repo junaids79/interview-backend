defe3fa7b0090b7cb71ccf24e29e6537a2f5b2f9
Junaid Shareef <junaids5954@gmail.com>
2026-03-29 14:44:36 +0530

    fix cors preflight

--- server.js
+++ server.js
@@ -7,21 +7,17 @@ const auth      = require('./routes/auth');
 
 const app = express();
 
-connectDB();
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://interview-frontend-wine.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
-  next();
-});
-
-app.use(cors({
+const corsOptions = {
   origin: 'https://interview-frontend-wine.vercel.app',
-  credentials: true
-}));
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
